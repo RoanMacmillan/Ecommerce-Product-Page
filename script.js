af8df74916e76110a686a1a33052bb642fec59ea
RoanMacmillan <hibee94@gmail.com>
2022-10-13 20:19:12 +0100

    added transitions to menus

--- script.js
+++ script.js
@@ -69,13 +69,15 @@ checkoutModal.classList.remove('active');
 // toggles mobile menu with hamburger btn
 
 const menuModal = document.querySelector('.modal');
+const sideMenu = document.querySelector('.nav-modal');
 
 document.querySelectorAll('.hamburger-btn').forEach((item) => {
 
   item.addEventListener('click', () => {
 
-    menuModal.classList.toggle('active');
 
+     menuModal.classList.toggle('transitionm');
+      sideMenu.classList.toggle('transitionwidth');
   })
 
 })
@@ -90,7 +92,75 @@ menuModal.addEventListener('click', (e) => {
 
 } else {
 
-menuModal.classList.remove('active');
+ menuModal.classList.toggle('transitionm');
+ sideMenu.classList.toggle('transitionwidth');
+
+
 
 }
+})
+
+
+
+
+// item counter // 
+
+let itemCount = 1;
+let itemCounter = document.getElementById('item-counter');
+
+document.getElementById('plus').addEventListener('click', () =>{
+
+  itemCount++;
+  itemCounter.innerHTML = itemCount;
+
+})
+
+document.getElementById('remove').addEventListener('click', () => {
+
+  if (itemCount === 0) {
+    return;
+  }
+  itemCount--;
+  itemCounter.innerHTML = itemCount;
+
+
+})
+
+const checkoutBtn = document.getElementById('anim-btn');
+const cartCounter = document.getElementById('notification');
+const sneakerAmt = document.getElementById('sneaker-amt');
+const totalPrice = document.getElementById('price-total');
+let sneakerPrice = 125;
+let cartCount = 0;
+
+checkoutBtn.addEventListener('click', () => {
+
+  
+  cartCount = cartCount + itemCount;
+
+  if (cartCount === 0) {
+
+    return;
+
+  }
+  document.getElementById('full').style.display = 'block';
+  document.getElementById('empty').style.display = 'none';
+
+    cartCounter.style.display = 'block';
+    cartCounter.innerHTML = cartCount;
+    sneakerAmt.innerHTML = cartCount;
+    totalPrice.innerText = cartCount * 125;
+})
+
+
+document.getElementById('trash').addEventListener('click', () => {
+
+document.getElementById('full').style.display = 'none';
+document.getElementById('empty').style.display = 'flex';
+cartCount = 0;
+
+cartCounter.innerHTML = 1;
+cartCounter.style.display = 'none';
+
+
 })
